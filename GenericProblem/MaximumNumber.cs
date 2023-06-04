8c3d99e3a638567bd4f070c3a02a1fb188dac551
Sayunkta Mankar <sayumankar77@gmail.com>
2023-06-04 19:31:41 +0530

    Add

--- GenericProblem/MaximumNumber.cs
+++ GenericProblem/MaximumNumber.cs
@@ -56,8 +56,34 @@ namespace GenericProblem
             }
             return 0;
         }
+        public static string MaxString(string firstNumber2, string secondNumber2, string thirdNumber2)
+        {
+            if (firstNumber2.CompareTo(secondNumber2) > 0 && firstNumber2.CompareTo(thirdNumber2) > 0 ||
+                firstNumber2.CompareTo(secondNumber2) >= 0 && firstNumber2.CompareTo(thirdNumber2) > 0 ||
+                firstNumber2.CompareTo(secondNumber2) > 0 && firstNumber2.CompareTo(thirdNumber2) >= 0)
+            {
+                return firstNumber2;
+            }
+
+            else if (secondNumber2.CompareTo(firstNumber2) > 0 && secondNumber2.CompareTo(thirdNumber2) > 0 ||
+                secondNumber2.CompareTo(firstNumber2) >= 0 && secondNumber2.CompareTo(thirdNumber2) > 0 ||
+                secondNumber2.CompareTo(firstNumber2) > 0 && secondNumber2.CompareTo(thirdNumber2) >= 0)
+            {
+                return secondNumber2;
+            }
+
+            else if (thirdNumber2.CompareTo(firstNumber2) > 0 && thirdNumber2.CompareTo(secondNumber2) > 0 ||
+                thirdNumber2.CompareTo(firstNumber2) >= 0 && thirdNumber2.CompareTo(secondNumber2) > 0 ||
+                thirdNumber2.CompareTo(firstNumber2) > 0 && thirdNumber2.CompareTo(secondNumber2) >= 0)
+            {
+                return thirdNumber2;
+            }
+            return null;
+        }
     }
 }
+    
+
     
 
     
