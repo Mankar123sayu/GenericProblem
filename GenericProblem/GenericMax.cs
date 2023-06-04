9541c9e688f93b7302b7a881f48fb2f3f310154b
Mankar123sayu <125425650+Mankar123sayu@users.noreply.github.com>
2023-06-04 19:27:42 +0530

    Update GenericMax.cs

--- GenericProblem/GenericMax.cs
+++ GenericProblem/GenericMax.cs
@@ -9,32 +9,7 @@ namespace GenericProblem
     public class GenericMax<T> where T : IComparable
     {
 
-
-        public static T Max(T firstNumber3, T secondNumber3, T thirdNumber3)
-        {
-            if (firstNumber3.CompareTo(secondNumber3) > 0 && firstNumber3.CompareTo(thirdNumber3) > 0 ||
-                firstNumber3.CompareTo(secondNumber3) >= 0 && firstNumber3.CompareTo(thirdNumber3) > 0 ||
-                firstNumber3.CompareTo(secondNumber3) > 0 && firstNumber3.CompareTo(thirdNumber3) >= 0)
-            {
-                return firstNumber3;
-            }
-
-            else if (secondNumber3.CompareTo(firstNumber3) > 0 && secondNumber3.CompareTo(thirdNumber3) > 0 ||
-                secondNumber3.CompareTo(firstNumber3) >= 0 && secondNumber3.CompareTo(thirdNumber3) > 0 ||
-                secondNumber3.CompareTo(firstNumber3) > 0 && secondNumber3.CompareTo(thirdNumber3) >= 0)
-            {
-                return secondNumber3;
-            }
-
-            else if (thirdNumber3.CompareTo(firstNumber3) > 0 && thirdNumber3.CompareTo(secondNumber3) > 0 ||
-                thirdNumber3.CompareTo(firstNumber3) >= 0 && thirdNumber3.CompareTo(secondNumber3) > 0 ||
-                thirdNumber3.CompareTo(firstNumber3) > 0 && thirdNumber3.CompareTo(secondNumber3) >= 0)
-            {
-                return thirdNumber3;
-            }
-            return firstNumber3;
-        }
-        public T firstNumber3, secondNumber3, thirdNumber3;
+       public T firstNumber3, secondNumber3, thirdNumber3;
 
         public GenericMax(T firstNumber3, T secondNumber3, T thirdNumber3)
         {
